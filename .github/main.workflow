f66a248039fe7551a409b2467ca96435c84c2eb1
Ahmed Bilal <ahbilal@microsoft.com>
2019-11-06 15:27:04 -0800

    updated

--- .github/main.workflow
+++ .github/main.workflow
@@ -9,6 +9,6 @@ action "Deploy to Azure" {
   env = {
     SERVICE_PRINCIPAL="http://testServiceName",
     TENANT_ID="72f988bf-86f1-41af-91ab-2d7cd011db47",
-    APPID="sdrasAzureAction"
+    APPID="abilalAzureAction"
   }
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http')
 
 var server = http.createServer(function(request, response) {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World 123!')
+  response.end('Hello World 123 !')
 })
 
 var port = process.env.PORT || 1337
